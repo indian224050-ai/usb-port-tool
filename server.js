6ee3bfe27194c3988ccd37c9f4dcd1f9253ccc3e
indian224050-ai <indian224050@gmail.com>
2026-07-06 21:50:46 +0545

    Update server.js

--- server.js
+++ server.js
@@ -8,20 +8,13 @@ app.use(express.json());
 
 /*
 ====================================
-MYSQL POOL CONNECTION
+MYSQL CONNECTION
 ====================================
 */
 
-const db = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+const db = mysql.createPool(
+  process.env.MYSQL_PUBLIC_URL
+);
 
 /*
 ====================================
